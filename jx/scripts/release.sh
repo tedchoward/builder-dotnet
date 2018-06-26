8c2acc0fdc295d9bf92d6cc8d45a770dc9377aef
Ted C. Howard <ted@tedchoward.com>
2018-06-26 17:06:19 -0500

    Creates initial Dockerfile for building dotnet

---
+++ jx/scripts/release.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+ORG=tedchoward
+APP_NAME=builder-dotnet
+VERSION=1.0.0
+
+docker build -t docker.io/$ORG/$APP_NAME:${VERSION} .
+docker push docker.io/$ORG/$APP_NAME:${VERSION}
+docker tag docker.io/$ORG/$APP_NAME:${VERSION} docker.io/$ORG/$APP_NAME:latest
+
+git tag -fa v${VERSION} -m "Release version ${VERSION}"
+git push origin v${VERSION}
